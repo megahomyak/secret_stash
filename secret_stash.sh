be138ac5a275ce94c112d73a19b140b715a8e101
megahomyak <g.megahomyak@gmail.com>
2025-07-23 02:05:29 +0500

    This is too tough, fuck this

--- secret_stash.sh
+++ secret_stash.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
-secret_doc_path="$1"
+set -u
+secret_doc_name="$1"
+secret_stash_remote_path="$secret_stash_remote_path"
+secret_stash_remote_host_name="$secret_stash_remote_host_name"
+secret_stash_local_path="$secret_stash_local_path"
+
 temp="$(mktemp)"
 (
 set -euo pipefail
 read -s -p "Enter the passphrase (hidden): " passphrase
 echo
-if [ -f "$secret_doc_path" ]; then
-    gpg --quiet --batch --yes --passphrase "$passphrase" --decrypt "$secret_doc_path" > "$temp"
+secret_doc_name_encrypted="$(export passphrase ; echo "$secret_doc_name" | openssl enc -aes-256-cbc -pass env:passphrase -pbkdf2 -nosalt | basenc --base64url)"
+secret_doc_remote_path_quoted="$(printf "%q" "$secret_stash_remote_path/$secret_doc_name_encrypted")"
+secret_doc_local_path="$secret_stash_local_path/$secret_doc_name_encrypted"
+mkdir -p "$secret_stash_local_path"
+if scp "$secret_stash_remote_host_name:"$secret_doc_remote_path_quoted "$secret_doc_local_path"; then
+    gpg --quiet --batch --yes --passphrase "$passphrase" --decrypt "$secret_doc_local_path" > "$temp"
 fi
 "$EDITOR" "$temp"
-gpg --quiet --symmetric --batch --yes --passphrase "$passphrase" --output "$secret_doc_path" "$temp"
+if awk "NF { exit 1 }" "$temp"; then
+    rm "$secret_doc_local_path" || true
+    ssh "$secret_stash_remote_host_name" "rm $secret_doc_remote_path_quoted" || true
+else
+    gpg --quiet --symmetric --batch --yes --passphrase "$passphrase" --output "$secret_doc_local_path" "$temp"
+    ssh "$secret_stash_remote_host_name" "mkdir -p $secret_doc_remote_path_quoted"
+    scp "$secret_doc_local_path" "$secret_stash_remote_host_name:"$secret_doc_remote_path_quoted
+fi
 )
 subshell_exit_code="$?"
 rm "$temp"
--- vim_pipe.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-exec 3> >(echo "<stdin>$(cat /dev/stdin)</stdin>")
-echo 123 | vim --not-a-term -c "w! /dev/fd/3" -
