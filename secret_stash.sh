0de8c9f26e6e82e2edef78e94b0288de8b25f538
megahomyak <g.megahomyak@gmail.com>
2025-07-23 20:23:01 +0500

    It's a success

--- secret_stash.sh
+++ secret_stash.sh
@@ -5,15 +5,16 @@ ssh_remote() { while ssh -o ConnectTimeout=${secret_stash_connect_timeout:-10} $
 cd $secret_stash_local_dir
 read -s -p "Enter the passphrase (hidden): " passphrase; echo
 fname=$(echo $query | openssl enc -aes-256-cbc -pass pass:$passphrase -pbkdf2 -nosalt | basenc --base64url)
-(temp_editing=$(mktemp); trap 'rm -f $temp_editing' EXIT
-    ssh_remote "mkdir -p $secret_stash_remote_dir && cat $secret_stash_remote_dir/$fname 2>/dev/null" | gpg --quiet --decrypt --batch --yes --passphrase $(cat key.txt) | gpg --quiet --decrypt --batch --yes --passphrase $passphrase --output $temp_editing || true
-    echo $fname
+(trap 'rm -f $temp_editing' EXIT; temp_editing=$(mktemp)
+    encrypt() { gpg --quiet --symmetric --batch --yes --passphrase "$1"; }
+    decrypt() { gpg --quiet --decrypt --batch --yes --passphrase "$1"; }
+    ssh_remote "mkdir -p $secret_stash_remote_dir && cat $secret_stash_remote_dir/$fname 2>/dev/null" | decrypt $(cat key.txt) | decrypt $passphrase > $temp_editing || true
     $EDITOR $temp_editing
     if awk 'NF { exit 1 }' $temp_editing; then
         rm -f $fname
         ssh_remote "rm -f $secret_stash_remote_dir/$fname"
     else
-        cat $temp_editing | gpg --quiet --symmetric --batch --yes --passphrase $(cat key.txt) | gpg --quiet --symmetric --batch --yes --passphrase $passphrase --output $fname
+        (encrypt $passphrase | encrypt $(cat key.txt)) < $temp_editing > $fname
         ssh_remote "cat > $secret_stash_remote_dir/$fname" < $fname
     fi
 )
