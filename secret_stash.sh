cb83750a7c4ef0cecd7e88fa40fe0a46a567020a
megahomyak <g.megahomyak@gmail.com>
2025-07-23 16:27:10 +0500

    Wanted to do "bad connectivity reconnects", realized they're probably better in the sending rather than in re-sending

--- secret_stash.sh
+++ secret_stash.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 set -euo pipefail
 # required vars: secret_stash_local_path, secret_stash_remote_host, secret_stash_remote_path
-read -p "Enter your query: " query
+query=test
+#read -s -p "Enter your query (hidden): " query; echo
 ssh_remote() { ssh $secret_stash_remote_host "$@"; }
 cd $secret_stash_local_path
-if [ "${query:0:1}" = "-" ]; then # Deletion
-    fname=${query:1}
-    rm $fname
-    ssh_remote "rm $secret_stash_remote_path/$fname"
-else # Possible creation -> editing
-    read -s -p "Enter the passphrase (hidden): " passphrase; echo
-    fname=$(echo $query | openssl enc -aes-256-cbc -pass pass:$passphrase -pbkdf2 -nosalt | basenc --base64url)
-    echo EDITING $fname
-    (
-        temp_editing=$(mktemp)
-        trap 'rm -f $temp_editing' EXIT
-        ( ssh_remote "mkdir -p $secret_stash_remote_path && cat $secret_stash_remote_path/$fname" | gpg --quiet --batch --yes --passphrase $passphrase --decrypt --output $temp_editing ) || true
-        $EDITOR $temp_editing
-        gpg --quiet --symmetric --batch --yes --passphrase $passphrase --output $fname $temp_editing
-    )
+passphrase=test
+#read -s -p "Enter the passphrase (hidden): " passphrase; echo
+encrypt_fname() { fname="$1"; echo $fname | openssl enc -aes-256-cbc -pass pass:$passphrase -pbkdf2 -nosalt | basenc --base64url; }
+send() {
+    fname="$1"
     ssh_remote "cat > $secret_stash_remote_path/$fname" < $fname
+}
+if [ "${query:0:1}" = '!' ]; then # Re-send; handy for when I have connectivity issues
+    send "$(encrypt_fname ${query:1})"
+else # (Try to download) and edit
+    fname=$(encrypt_fname $query)
+    temp_editing=$(mktemp); trap 'rm -f $temp_editing' EXIT
+    ( ssh_remote "mkdir -p $secret_stash_remote_path && cat $secret_stash_remote_path/$fname 2&>/dev/null" | gpg --quiet --batch --yes --passphrase $passphrase --decrypt --output $temp_editing ) || true
+    $EDITOR $temp_editing
+    if awk 'NF { exit 1 }' $temp_editing; then
+        rm -f $fname
+        ssh_remote "rm -f $secret_stash_remote_path/$fname"
+    else
+        gpg --quiet --symmetric --batch --yes --passphrase $passphrase --output $fname $temp_editing
+        echo 
+        send $fname
+    fi
 fi
