f827501b7cd09512a835e58a0d7175b82f24a338
megahomyak <g.megahomyak@gmail.com>
2025-07-23 18:52:56 +0500

    mkdir -p $secret_stash_local_dir

--- secret_stash.sh
+++ secret_stash.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 read -s -p "Enter your query (hidden): " query; echo
 ssh_remote() { while ssh -o ConnectTimeout=${secret_stash_connect_timeout:-10} $secret_stash_remote_host "$@"; [ $? = 255 ]; do echo Reconnecting... >&2; done }
+mkdir -p $secret_stash_local_dir
 cd $secret_stash_local_dir
 read -s -p "Enter the passphrase (hidden): " passphrase; echo
 fname=$(echo $query | openssl enc -aes-256-cbc -pass pass:$passphrase -pbkdf2 -nosalt | basenc --base64url)
