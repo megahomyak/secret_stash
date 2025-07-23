8e014012eda6b6e4b38e5557ed4536c99e36c34a
megahomyak <g.megahomyak@gmail.com>
2025-07-23 16:05:41 +0500

    Separate creation and deletion, but it's really shitty now

--- secret_stash.sh
+++ secret_stash.sh
@@ -1,32 +1,23 @@
 #!/bin/bash
-set -u
-secret_doc_name="$1"
-secret_stash_remote_path="$secret_stash_remote_path"
-secret_stash_remote_host_name="$secret_stash_remote_host_name"
-secret_stash_local_path="$secret_stash_local_path"
-
-temp="$(mktemp)"
-(
 set -euo pipefail
-read -s -p "Enter the passphrase (hidden): " passphrase
-echo
-secret_doc_name_encrypted="$(echo "$secret_doc_name" | openssl enc -aes-256-cbc -pass pass:$passphrase -pbkdf2 -nosalt | basenc --base64url)"
-secret_doc_remote_path_quoted="$(printf "%q" "$secret_stash_remote_path/$secret_doc_name_encrypted")"
-mkdir -p "$secret_stash_local_path"
-cd "$secret_stash_local_path"
-if scp "$secret_stash_remote_host_name:"$secret_doc_remote_path_quoted "$secret_doc_local_path"; then
-    gpg --quiet --batch --yes --passphrase "$passphrase" --decrypt "$secret_doc_local_path" > "$temp"
+# required vars: secret_stash_local_path, secret_stash_remote_host, secret_stash_remote_path
+read -p "Enter your query: " query
+ssh_remote() { ssh $secret_stash_remote_host "$@"; }
+cd $secret_stash_local_path
+if [ "${query:0:1}" = "-" ]; then # Deletion
+    fname=${query:1}
+    rm $fname
+    ssh_remote "rm $secret_stash_remote_path/$fname"
+else # Possible creation -> editing
+    read -s -p "Enter the passphrase (hidden): " passphrase; echo
+    fname=$(echo $query | openssl enc -aes-256-cbc -pass pass:$passphrase -pbkdf2 -nosalt | basenc --base64url)
+    echo EDITING $fname
+    (
+        temp_editing=$(mktemp)
+        trap 'rm -f $temp_editing' EXIT
+        ( ssh_remote "mkdir -p $secret_stash_remote_path && cat $secret_stash_remote_path/$fname" | gpg --quiet --batch --yes --passphrase $passphrase --decrypt --output $temp_editing ) || true
+        $EDITOR $temp_editing
+        gpg --quiet --symmetric --batch --yes --passphrase $passphrase --output $fname $temp_editing
+    )
+    ssh_remote "cat > $secret_stash_remote_path/$fname" < $fname
 fi
-"$EDITOR" "$temp"
-if awk "NF { exit 1 }" "$temp"; then
-    rm "$secret_doc_local_path" || true
-    ssh "$secret_stash_remote_host_name" "rm $secret_doc_remote_path_quoted" || true
-else
-    gpg --quiet --symmetric --batch --yes --passphrase "$passphrase" --output "$secret_doc_local_path" "$temp"
-    ssh "$secret_stash_remote_host_name" "mkdir -p $secret_doc_remote_path_quoted"
-    scp "$secret_doc_local_path" "$secret_stash_remote_host_name:"$secret_doc_remote_path_quoted
-fi
-)
-subshell_exit_code="$?"
-rm "$temp"
-exit "$subshell_exit_code"
